baa78863488407cb03a688237b37065c8508e1cf
Rodrigo Barros <digaodev07@gmail.com>
2024-10-24 12:47:03 -0300

    alterações adicionando rota list fora da rota pai

--- src/app/music/music.service.ts
+++ src/app/music/music.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class MusicService {
 
   private httpOptions = {
     headers: new HttpHeaders({
-      'Cache-Control': 'no-cache',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
       'Pragma': 'no-cache',
       'Expires': '0'
     })
@@ -20,15 +20,7 @@ export class MusicService {
   // GET: Listar todas as músicas
   getMusics(): Observable<Music[]> {
     const url = `${this.apiUrl}?_=${new Date().getTime()}`;
-    return this.http.get<Music[]>(this.apiUrl, this.httpOptions).pipe(
-      tap(response => {
-        console.log('Resposta da API:', response);
-      }),
-      catchError(error => {
-        console.error('Erro na requisição GET:', error);
-        return throwError(error);
-      })
-    );
+    return this.http.get<Music[]>(this.apiUrl, this.httpOptions)
   }
 
   // POST: Criar uma nova música
